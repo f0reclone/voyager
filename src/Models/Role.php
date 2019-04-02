28fb2e3378b37cf3a28fa90b23cf569f72f882b1
Andreas Neufeld <aneufeld@drivebeyond.eu>
2019-04-02 14:41:16 +0200

    role change and translations

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,6 +2,8 @@
 
 namespace TCG\Voyager\Models;
 
+use Auth;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use TCG\Voyager\Facades\Voyager;
 
@@ -21,5 +23,20 @@ class Role extends Model
     public function permissions()
     {
         return $this->belongsToMany(Voyager::modelClass('Permission'));
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('available', function (Builder $builder) {
+            //$id = Auth::user()->role_id;
+            //$builder->where('id', '<=', $id);
+            if (Auth::user() != null) {
+                $rang = Auth::user()->getrole->rang;
+                $builder->where('rang', '>=', $rang);
+            }
+        });
+
     }
 }
