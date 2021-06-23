29612f1c6becdf131a484898d680701b8a52ed4e
Andreas Neufeld <andreas.neufeld@aboutyou.com>
2021-06-24 01:43:03 +0200

    update tinymce

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -1,4 +1,15 @@
-$(document).ready(function () {
+// Theme
+import 'tinymce/themes/modern/theme';
+
+// Plugins
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/table';
+import 'tinymce/plugins/textcolor';
+import 'tinymce/plugins/lists';
+
+$(document).ready(function(){
 
     $.ajaxSetup({
         headers: {
@@ -8,18 +19,18 @@ $(document).ready(function () {
 
     tinymce.init({
         menubar: false,
-        selector: 'textarea.richTextBox',
-        skin: 'voyager',
+        selector:'textarea.richTextBox',
+        skin_url: $('meta[name="assets-path"]').attr('content')+'?path=js/skins/voyager',
         min_height: 600,
         resize: 'vertical',
-        plugins: 'link, image, code, youtube, giphy, table, textcolor, lists',
-        extended_valid_elements: 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-        file_browser_callback: function (field_name, url, type, win) {
-            if (type == 'image') {
+        plugins: 'link, image, code, table, textcolor, lists',
+        extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
+        file_browser_callback: function(field_name, url, type, win) {
+            if(type =='image'){
                 $('#upload_file').trigger('click');
             }
         },
-        toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table youtube giphy | code',
+        toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table | code',
         convert_urls: false,
         image_caption: true,
         image_title: true,
@@ -28,10 +39,11 @@ $(document).ready(function () {
                 tinymce_init_callback(editor);
             }
         },
-        image_dimensions: false,
-        image_class_list: [
-            {title: 'Responsive', value: 'img-fluid'}
-        ]
+        setup: function (editor) {
+            if (typeof tinymce_setup_callback !== "undefined") {
+                tinymce_setup_callback(editor);
+            }
+        }
     });
 
 });
