5ec3c4e05ae3679f6bde880ba5b3f84f1401b21e
Andreas Neufeld <andreas.neufeld@aboutyou.com>
2021-06-23 22:37:00 +0200

    update tinymce

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -1,15 +1,4 @@
-// Theme
-import 'tinymce/themes/modern/theme';
-
-  // Plugins
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/image';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/table';
-import 'tinymce/plugins/textcolor';
-import 'tinymce/plugins/lists';
-
-$(document).ready(function(){
+$(document).ready(function () {
 
     $.ajaxSetup({
         headers: {
@@ -17,31 +6,26 @@ $(document).ready(function(){
         }
     });
 
-  tinymce.init({
-    menubar: insert,
-    selector:'textarea.richTextBox',
-    skin_url: $('meta[name="assets-path"]').attr('content')+'?path=js/skins/voyager',
-    min_height: 600,
-    resize: 'vertical',
-    plugins: 'link, image, code, table, textcolor, lists',
-    extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-    file_browser_callback: function(field_name, url, type, win) {
-            if(type =='image'){
-              $('#upload_file').trigger('click');
+    tinymce.init({
+        menubar: false,
+        selector: 'textarea.richTextBox',
+        skin: 'voyager',
+        min_height: 600,
+        resize: 'vertical',
+        plugins: 'link, image, code, youtube, giphy, table, textcolor, lists',
+        extended_valid_elements: 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
+        file_browser_callback: function (field_name, url, type, win) {
+            if (type == 'image') {
+                $('#upload_file').trigger('click');
             }
         },
-        toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image media table | code',
+        toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table youtube giphy | code',
         convert_urls: false,
         image_caption: true,
         image_title: true,
         init_instance_callback: function (editor) {
             if (typeof tinymce_init_callback !== "undefined") {
                 tinymce_init_callback(editor);
-            }
-        },
-        setup: function (editor) {
-            if (typeof tinymce_setup_callback !== "undefined") {
-                tinymce_setup_callback(editor);
             }
         },
         image_dimensions: false,
